3aafb189317a568c6214ef6a560fa870e8674802
Alexc <alexsejsi4@gmail.com>
2022-11-04 15:53:02 +0200

    Initial commit

---
+++ js/JS-basics.js
@@ -0,0 +1,45 @@
+//Таски на змінні
+//1
+const a = 3;
+const b = 4;
+console.log("a*b", a * b);
+//2
+console.log("a/b", a / b);
+//3
+console.log("a+b", a + b);
+//4
+const c = 11;
+const d = true;
+const e = "java script";
+const f = "100";
+console.log("11", c);
+console.log("true", d);
+console.log("java script", e);
+console.log("100", f);
+//5
+let num = 1;
+num += 11;
+num -= 11;
+num *= 11;
+num /= 11;
+num++;
+num--;
+//Таски на prompt
+//1
+const userNumber = Number(prompt("Enter your number"));
+//2
+const userNumber1 = Number(prompt("Enter your first number"));
+const userNumber2 = Number(prompt("Enter your second number"));
+//3
+const userTime = Number(prompt("Enter your time"));
+//4
+const greeting = "Hello, ";
+const userName = prompt("Enter your name");
+//1
+alert(userNumber ** 2);
+//2
+alert((userNumber1 + userNumber2) / 2);
+//3
+alert(userTime * 60);
+//4
+alert(greeting + userName + "!");
